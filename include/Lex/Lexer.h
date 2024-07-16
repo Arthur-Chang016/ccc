ace474a91cc20d3e69ab01f6113b7a3dd88ff340
Arthur-Chang016 <b05505016@g.ntu.edu.tw>
2024-07-16 01:30:55 -0700

    Merge LocSV and Lexer. Refactor

--- include/Lex/Lexer.h
+++ include/Lex/Lexer.h
@@ -22,7 +22,7 @@ class Loc {
     std::string toString() const;
 };
 
-class LocSV;  // TODO, move it here
+// class LocSV;  // TODO, move it here
 
 enum streamMode {
     LONG_COMMENT,
@@ -31,18 +31,83 @@ enum streamMode {
     NORMAL
 };
 
-class Lexer {
-    // std::string source;
-    std::vector<Token> tokens;
-
-    // TODO change to array of tokens
-
-    void buildTokenStream(LocSV input);
+class Lexer : std::string_view {
+    /**
+     * Assume the start point of the input can form a new token
+     */
+    Lexer buildTokenStream();
 
     void reportAndHalt(const Loc loc, const std::string &msg);
 
    public:
-    Lexer(std::string s);
+    Lexer(std::string_view s);
+
+    using super = std::string_view;
+
+   private:
+    int line, offset;
+
+   public:
+    Lexer(int l, int o, std::string_view sv)
+        : line(l), offset(o), super(sv) {}
+
+    Lexer consume(size_t n) {
+        if (n == 0 || this->empty()) return *this;
+        if (this->at(0) == '\n')
+            return Lexer(line + 1, 1, this->substr(1)).consume(n - 1);
+        else
+            return Lexer(line, offset + 1, this->substr(1)).consume(n - 1);
+    }
+
+   private:
+    /**
+     * Keep deleting prefix until long comment ends or EOF
+     */
+    Lexer consumeLongComment();
+
+    /**
+     * keep deleting prefix until end of line or EOF
+     */
+    Lexer consumeShortComment();
+
+    /**
+     * take the incoming decimal leteral
+     */
+    Lexer consumeDeciLit(int64_t &retInt);
+
+    /**
+     * take the incoming hex leteral
+     */
+    Lexer consumeHexLit(int64_t &retInt);
+
+    Lexer consumeHexLitInner(int64_t &retInt);
+
+    Lexer consumeSingleChar(int64_t &retInt);
+
+    /**
+     * take the incoming char leteral
+     * already consume the first '
+     */
+    Lexer consumeCharLit(int64_t &retInt);
+
+    /**
+     * In string literal mode, keep building 'retStrLit' until "
+     * supported special case: \ with n, t, ', ", \, and decimal literals
+     */
+    Lexer consumeStrLit(std::string &retStrLit);
+
+    /**
+     * symbol can starts with alphabet followed with numbers
+     */
+    Lexer consumeSymbol(std::string &symbol);
+
+    Lexer consumeStrLitInner(std::string &retStrLit);
+
+    bool startWithHexLit();
+
+    bool startsWithSign();
+
+    bool startsWithSymbol();
 };
 
 }  // namespace ccc
--- lib/Lex/Lexer.cpp
+++ lib/Lex/Lexer.cpp
@@ -9,83 +9,39 @@
 
 namespace ccc {
 
-// TODO change LocSV to Lexer, they can be merged
-
-/**
- * Inherit from std::string_view, with loc information
- * Will update loc informantion when doing substr()
- */
-class LocSV : public std::string_view {
-    using super = std::string_view;
-
-   private:
-    int line, offset;
-
-   public:
-    LocSV(int l, int o, std::string_view sv)
-        : line(l), offset(o), super(sv) {}
-
-    // LocSV() : line(1), offset(1) {}
-
-    LocSV consume(size_t n) {
-        if (n == 0 || this->empty()) return *this;
-        if (this->at(0) == '\n')
-            return LocSV(line + 1, 1, this->substr(1)).consume(n - 1);
-        else
-            return LocSV(line, offset + 1, this->substr(1)).consume(n - 1);
-    }
-
-    bool startsWithSign();
-
-    bool startsWithSymbol();
-};
-
 Loc::Loc(int l, int o) : line(l), offset(o) {}
 
 std::string Loc::toString() const {
     return "[" + std::to_string(line) + ", " + std::to_string(offset) + "]";
 }
 
-Lexer::Lexer(std::string s) {
-    LocSV source(1, 1, s);
-    buildTokenStream(source);
-}
+Lexer::Lexer(std::string_view s) : super(s) {}
 
 void Lexer::reportAndHalt(const Loc loc, const std::string &msg) {
     std::cerr << "Lex Error" << loc.toString() << " : " << msg << std::endl;
     exit(1);
 }
 
-/**
- * Keep deleting prefix until long comment ends or EOF
- */
-LocSV consumeTilLongCommentEnds(LocSV input) {
-    if (input.size() < 2) return input.consume(input.size());
-    if (input.substr(0, 2) != "*/")
-        return consumeTilLongCommentEnds(input.consume(1));
+Lexer Lexer::consumeLongComment() {
+    if (this->size() < 2) return this->consume(this->size());
+    if (this->substr(0, 2) != "*/")
+        return this->consume(1).consumeLongComment();
     else
-        return input.consume(2);  // eliminate long comment end
+        return this->consume(2);  // eliminate long comment end
 }
 
-/**
- * keep deleting prefix until end of line or EOF
- */
-LocSV consumeTilShortCommentEnds(LocSV input) {
-    if (input.empty()) return input;
-    if (input.at(0) != '\n')
-        return consumeTilShortCommentEnds(input.consume(1));
+Lexer Lexer::consumeShortComment() {
+    if (this->empty()) return *this;
+    if (this->at(0) != '\n')
+        return this->consume(1).consumeShortComment();
     else
-        return input.consume(1);  // eliminate short comment end
+        return this->consume(1);  // eliminate short comment end
 }
 
-/**
- * take the incoming decimal leteral
- */
-LocSV consumeDeciLit(LocSV input, int64_t &retInt) {
-    // assert(std::isdigit(input.at(0)) && "input should start with digit");
-    if (input.empty() || std::isdigit(input.at(0)) == false) return input;
-    char num = input.at(0) - '0';
-    return consumeDeciLit(input.consume(1), retInt = (retInt * 10) + num);
+Lexer Lexer::consumeDeciLit(int64_t &retInt) {
+    if (this->empty() || std::isdigit(this->at(0)) == false) return *this;
+    char num = this->at(0) - '0';
+    return this->consume(1).consumeDeciLit(retInt = (retInt * 10) + num);
 }
 
 char getHexNum(const char c) {
@@ -99,23 +55,23 @@ bool isHexNum(const char c) {
     return std::isdigit(c) || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F');
 }
 
-/**
- * take the incoming hex leteral
- */
-LocSV consumeHexLit(LocSV input, int64_t &retInt) {
-    // assert((input.starts_with("0x") || input.starts_with("0X")) && "input should start with 0x");
-    if (input.empty() || isHexNum(input.at(0)) == false) return input;
-    // char num = input.at(0) - '0';
-    char num = getHexNum(input.at(0));
-    return consumeHexLit(input.consume(1), retInt = (retInt * 16) + num);
+Lexer Lexer::consumeHexLit(int64_t &retInt) {
+    assert(this->startWithHexLit() && "Should start with 0x or 0X");
+    return this->consumeHexLitInner(retInt = 0);
 }
 
-LocSV consumeSingleChar(LocSV input, int64_t &retInt) {
-    if (input.at(0) == '\\') {
-        if (input.size() <= 1) {
+Lexer Lexer::consumeHexLitInner(int64_t &retInt) {
+    if (this->empty() || isHexNum(this->at(0)) == false) return *this;
+    char num = getHexNum(this->at(0));
+    return this->consume(1).consumeHexLitInner(retInt = (retInt * 16) + num);
+}
+
+Lexer Lexer::consumeSingleChar(int64_t &retInt) {
+    if (this->at(0) == '\\') {
+        if (this->size() <= 1) {
             // TODO throw, need after reverse slash
         }
-        switch (input.at(1)) {
+        switch (this->at(1)) {
             case 'n':
                 retInt = '\n';
                 break;
@@ -132,59 +88,55 @@ LocSV consumeSingleChar(LocSV input, int64_t &retInt) {
                 retInt = '\\';
                 break;
             default:
-                if (std::isdigit(input.at(1))) {
+                if (std::isdigit(this->at(1))) {
                     int64_t intLit = 0;
-                    input = consumeDeciLit(input, intLit);
+                    Lexer retPoint = consumeDeciLit(intLit);
+
                     // TODO build char with given ascii num (truncated)
+                    return retPoint;
                 } else {
                     // TODO throw unsupported reverse slash command
                 }
         }
-        return input.consume(2);
+        return this->consume(2);
     } else {
-        retInt = input.at(0);
-        return input.consume(1);
+        retInt = this->at(0);
+        return this->consume(1);
     }
 }
 
-/**
- * take the incoming char leteral
- * already consume the first '
- */
-LocSV consumeCharLit(LocSV input, int64_t &retInt) {
-    if (input.empty()) {
-        // TODO throw, no following closed char
-    }
-    input = consumeSingleChar(input, retInt);
-    if (input.starts_with('\'') == false) {
+Lexer Lexer::consumeCharLit(int64_t &retInt) {
+    assert(this->starts_with('\'') && "Should start with '");
+    Lexer retLex = this->consume(1).consumeSingleChar(retInt = 0);
+    if (this->starts_with('\'') == false) {
         // TODO throw, incompleted char lit
     }
-    return input.consume(1);  // consume the second '
+    return retLex.consume(1);  // consume the second '
 }
 
-/**
- * In string literal mode, keep building 'retStrLit' until "
- * supported special case: \ with n, t, ', ", \, and decimal literals
- */
-LocSV consumeTileStrLitEnds(LocSV input, std::string &retStrLit) {
-    if (input.empty()) {
+Lexer Lexer::consumeStrLit(std::string &retStrLit) {
+    assert(retStrLit.starts_with("\"") && "Should start with \"");
+    return this->consume(1).consumeStrLitInner(retStrLit = {});
+}
+
+Lexer Lexer::consumeStrLitInner(std::string &retStrLit) {
+    if (this->empty()) {
         // TODO throw lexer error, should close the string
     }
-    if (input.at(0) == '\n') {
+    if (this->at(0) == '\n') {
         // TODO throw, string should end at the same line
     }
-
-    if (input.at(0) == '\"') return input.consume(1);
+    if (this->at(0) == '\"') return this->consume(1);
     // normal char
     int64_t charLit = 0;
-    input = consumeSingleChar(input, charLit);
+    Lexer retLex = consumeSingleChar(charLit);
     retStrLit += char(charLit);
-    return consumeTileStrLitEnds(input, retStrLit);
+    return retLex.consumeStrLitInner(retStrLit);
 }
 
 // std::vector<std::string> signs{"{", "}", ";", ":", "*", "(", ")"};
 
-bool LocSV::startsWithSign() {
+bool Lexer::startsWithSign() {
     if (this->starts_with("{")) {
         return true;
     } else if (this->starts_with("}")) {
@@ -337,61 +289,55 @@ bool isReservedSymbol(const std::string &symbol) {
     return false;
 }
 
-/**
- * symbol can starts with alphabet followed with numbers
- */
-LocSV consumeSymbol(LocSV input, std::string &symbol) {
-    if (input.empty() || canBeSymbol(input.at(0)) == false) return input;
-    return consumeSymbol(input.consume(1), symbol += input.at(0));
+Lexer Lexer::consumeSymbol(std::string &symbol) {
+    if (this->empty() || canBeSymbol(this->at(0)) == false) return *this;
+    return this->consume(1).consumeSymbol(symbol += this->at(0));
 }
 
 // TODO check all substr
 
-/**
- * Assume the start point of the input can form a new token
- */
-void Lexer::buildTokenStream(LocSV input) {
-    if (input.empty()) return;
-    char cur = input.at(0);
+bool Lexer::startWithHexLit() {
+    return this->starts_with("0x") || this->starts_with("0X");
+}
 
-    if (input.starts_with("/*")) {  // long comment starts
-        input = consumeTilLongCommentEnds(input);
-        buildTokenStream(input);
-    } else if (input.starts_with("//")) {  // short comment starts
-        input = consumeTilShortCommentEnds(input);
-        buildTokenStream(input);
+Lexer Lexer::buildTokenStream() {
+    if (this->empty()) return *this;
+    char cur = this->at(0);
+
+    if (this->starts_with("/*")) {  // long comment starts
+        return this->consumeLongComment().buildTokenStream();
+    } else if (this->starts_with("//")) {  // short comment starts
+        return this->consumeShortComment().buildTokenStream();
     } else if (cur == '\"') {  // string lit start
         std::string strLit;
-        input = consumeTileStrLitEnds(input.consume(1), strLit);
+        Lexer retLex = this->consumeStrLit(strLit);
         // TODO build str lit token
-        buildTokenStream(input);
+        return retLex.buildTokenStream();
     } else if (cur == '\'') {
         int64_t charLit = 0;
-        input = consumeCharLit(input.consume(1), charLit);
+        Lexer retLex = this->consumeCharLit(charLit);
         // TODO build char lit token
+        return retLex.buildTokenStream();
     } else if (std::isdigit(cur)) {  // int literal
         int64_t intLit = 0;
-        if (input.starts_with("0x") || input.starts_with("0X")) {  // hex
-            input = consumeHexLit(input.consume(2), intLit);       // skip 0x
-        } else {                                                   // decimal
-            input = consumeDeciLit(input, intLit);
-        }
+        Lexer retLex = this->startWithHexLit() ? this->consumeHexLit(intLit) : this->consumeDeciLit(intLit);
         // TODO build int lit token
-        buildTokenStream(input);
-    } else if (input.startsWithSign()) {
+        return retLex.buildTokenStream();
+    } else if (this->startsWithSign()) {
+        // TODO consume sign
         // TODO build sign token
 
-    } else if (std::isalpha(cur) || cur == '_') {
+    } else if (std::isalpha(cur) || cur == '_') {  // symbol
         std::string symbol;
-        input = consumeSymbol(input, symbol);
+        Lexer retLex = this->consumeSymbol(symbol);
         if (isReservedSymbol(symbol)) {
             // TODO build keyword token
         } else {
             // TODO build ID token
         }
-        buildTokenStream(input);
+        return retLex.buildTokenStream();
     } else if (std::isspace(cur)) {
-        buildTokenStream(input.consume(1));
+        return this->consume(1).buildTokenStream();
     } else {
         // TODO throw unsupported input
     }
